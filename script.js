2c911dea26d8c135bc201deee9d824c643297f4d
Bianca Lima <66188115+Bianca-bit@users.noreply.github.com>
2023-03-13 18:32:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let value = {
+    nome:'',
+    URL: '',
+    subject:'',
+    
+}
+console.log(value);
+
+function Personagem (nome){
+    this.nome = '',
+    this.url = ''
+    this.subject = '';
+};
+
+console.log(Personagem);
+
