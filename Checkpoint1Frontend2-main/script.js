28d2f305fd26ba204e04cbc3af022ef241f75d98
Bianca Lima <66188115+Bianca-bit@users.noreply.github.com>
2023-03-14 22:30:44 -0300

    Add files via upload

---
+++ Checkpoint1Frontend2-main/script.js
@@ -0,0 +1,22 @@
+/*let value = {
+    nome:'',
+    URL: '',
+    subject:'',
+    
+}
+console.log(value);
+
+function Personagem (nome){
+    this.nome = '',
+    this.url = ''
+    this.subject = '';
+};
+
+console.log(Personagem);*/
+
+let formulario = document.querySelector("form");
+formulario.addEventListener("submit", function(event) {
+   event.preventDefault()
+})
+
+
